f71d21673de1afd64e540daf87f61475d449be40
Sai <main@example.com>
2017-02-03 18:15:27 +0400

    Git  add modules

--- Module.php
+++ Module.php
@@ -10,7 +10,7 @@ class Module extends \yii\base\Module
     /**
      * @inheritdoc
      */
-    public $controllerNamespace = 'frontend\modules\portfolio\controllers';
+    public $controllerNamespace = 'omcrn\portfolio\controllers';
 
     /**
      * @inheritdoc
